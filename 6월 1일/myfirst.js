3c49d74ff1501174c7bcac4908456ed30acf9c81
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-06-01 14:56:56 +0900

    Update myfirst.js
    t

--- 6월 1일/myfirst.js
+++ 6월 1일/myfirst.js
@@ -2,6 +2,7 @@ var http = require('http');
 var dt = require('./myfirstmodule')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html;charset=utf8'});
+  res.writeHead(200, {'Content-Type': 'text/html;charset=utf8',
+                     'myNameIs':'dae young'});
   res.end('Hello World! : '+dt.myDateTime());
 }).listen(8080);
